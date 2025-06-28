994133abb9e98f7882c4628ec1fcfd471db7fb0c
fazalasim321 <tahiratousef7@gmail.com>
2025-06-28 04:43:38 -0700

    Add files via upload

---
+++ ai hub part 1.py
@@ -0,0 +1,47 @@
+#Create a variable greeting and store a message. Print it.
+greeting = ("fazal asim")
+print(greeting)
+#Change the value of greeting and print the new message.
+greeting = ("I love python")
+print(greeting)
+# Create first_name and last_name, then print full name using f-string.
+first_name = "fazal"
+last_name = "asim"
+print(f"{first_name}{last_name}")
+#Print a famous quote with author’s name using f-string.
+quote = "work , work and work"
+author = "Quaid azam"
+print(f"{author} once said,'{quote}'")
+#Store a name with extra spaces, strip them, and print clean output.
+name_with_spaces = "fazal asim"
+print(name_with_spaces.strip())
+#Take a number, add 5, multiply by 2, subtract 3, and print the result.
+number = 10
+result = (10 + 5) * 2- 3
+print(result)
+#Create a and b; print their sum, difference, product, and quotient.
+a = 5
+b = 6
+
+print(f"sum:{a + b}")
+print(f"Difference:{a - b}")
+print(f"product:{a * b}")
+print(f"quotient:{a / b}")
+#Find square and cube of a number using ** operator.
+num = 3
+print(f"square:{num ** 2}")
+print(f"cube:{num ** 3}")
+#Add three floating-point numbers and print the total.
+f1 = 2.2
+f2 = 3.5
+f3 = 5.8
+total = f1+f2+f3
+print(total)
+#Assign x, y, z in one line and print them.
+x, y ,z = 1, 2, 3
+print(x, y, z)
+#Create a list of 5 favorite fruits and print each one separately.
+fruits =["Apple","Banana","Cheery","Mango","Orange"]
+for fruit in fruits:
+    print(fruits) 
+ 
