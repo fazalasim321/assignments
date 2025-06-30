ea32e989049f4d9d95c95dde9bb51218eeebf874
fazalasim321 <tahiratousef7@gmail.com>
2025-06-29 22:36:11 -0700

    Add files via upload
    🧠 Python Basics Practice – Set 01 🔰
    (Beginner Level)

---
+++ no # 1 .py
@@ -0,0 +1,35 @@
+#print your name
+print("fazal asim")
+#printyour age
+age = 18
+print(age)
+#print your school name
+school_name = "al yousaf public school sohdra"
+print(school_name)
+ # Add two numbers using variables and print the result.
+num1 = 15
+num2 = 67
+result = num1 + num2
+print(result)
+ # Store your name in a variable and print a welcome message.
+name = "fazal asim"
+print("Welcome,",name)
+# Declare a variable for your city and print where you live.
+city = "Lahore"
+print("I live in", city)
+ #Use multiple variables (name, age, city) and print a full sentence.
+print("My name is", name + ", I am", age, "years old and I live in", city + ".")
+
+#Perform subtraction between two numbers using variables and print the answer.
+a = 50
+b = 20
+sub_result = a - b
+print("Subtraction:", sub_result)
+
+#Store a motivational quote in a variable and print it.
+quote = "Success is not final, failure is not fatal: It is the courage to continue that counts."
+print("Motivational Quote:", quote)
+
+#Create a variable for your favorite subject and print it.
+favorite_subject = "Python Programming"
+print("My favorite subject is:", favorite_subject)
