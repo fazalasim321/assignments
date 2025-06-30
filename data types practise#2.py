465ebb9db0080c174edd655f277cc45769eb37e6
fazalasim321 <tahiratousef7@gmail.com>
2025-06-29 22:39:27 -0700

    Add files via upload
    📘 Python Assignment – Data Types Practice

---
+++ data types practise#2.py
@@ -0,0 +1,63 @@
+#Declare a variable named age and assign it an integer value 25.
+#Print the value of age and its data type using the type() function.
+age = "25"
+print("Age :",age)
+print("Type of age:",type(age))
+#Create a variable called temperature and assign it a float value 98.6.
+# Print the value of temperature and its data type.
+temperature = 98.6
+print("\nTemperature:", temperature)
+print("Type of temperature:", type(temperature))
+#Declare a variable complex_num and assign it a complex number 3 + 4j.
+# Print the value of complex_num and its data type.
+complex_num = 3 + 4j
+print("\nComplex Number:", complex_num)
+print("Type of complex_num:", type(complex_num))
+#Create a variable named is_student and assign it the boolean value True.
+# Print the value of is_student and its data type.
+is_student = True
+print("\nIs student:", is_student)
+print("Type of is_student:", type(is_student))
+ #Declare a variable called has_job and assign it the boolean value False.
+# Print the value of has_job and its data type.
+has_job = False
+print("\nHas job:", has_job)
+print("Type of has_job:", type(has_job))
+ #Create a variable greeting and assign it the string 'Hello' using single quotes.
+# Print the value of greeting and its data type.
+greeting = 'Hello'
+print("\nGreeting:", greeting)
+print("Type of greeting:", type(greeting))
+#Declare a variable named city and assign it the string "New York" using double quotes.
+# Print the value of city and its data type.
+city = "New York"
+print("\nCity:", city)
+print("Type of city:", type(city))
+
+# 8️⃣ Create a variable quote and assign it the string 'Life is short' using triple single quotes.
+# Print the value of quote and its data type.
+quote = '''Life is short'''
+print("\nQuote:", quote)
+print("Type of quote:", type(quote))
+# Declare a variable called poem and assign it a multi-line string using triple double quotes.
+# Text:
+# Roses are red,
+# Violets are blue.
+# Print the value of poem and its data type.
+poem = """Roses are red,
+Violets are blue."""
+print("\nPoem:\n" + poem)
+print("Type of poem:", type(poem))
+#  Create 3 variables:
+# score as an integer with value 95
+# height as a float with value 5.9
+# name as a string with value "Alice"
+# Print the value and data type of each variable.
+
+score = 95
+height = 5.9
+name = "Alice"
+
+print("\nScore:", score, "| Type:", type(score))
+print("Height:", height, "| Type:", type(height))
+print("Name:", name, "| Type:", type(name))
